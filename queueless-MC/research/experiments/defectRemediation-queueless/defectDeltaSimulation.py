cb5f1f783c27df9a97d695719a394844c362c3ea
mdebeurre <mdebeurre@radiantlogic.com>
2025-07-15 14:34:08 +0200

    Folder restructuring: problem launching jupyter notebooks with current organization, name too long. Moved under research/

--- queueless-MC/research/experiments/defectRemediation-queueless/defectDeltaSimulation.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-class defectRemediationDeltaSimulator:
-    def __init__(self, t_end):
-        self.t_end = t_end
-
-    
--- queueless-MC/research/experiments/defectRemediation-queueless/initialize_logs.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def initialize_logs():
-    logs_df = pd.read_pickle('logs_df.pkl') # existing df
-    
