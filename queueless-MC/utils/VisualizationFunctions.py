5855f8a36b0695e81e3be5961ffad9a01b016439
mdebeurre <mdebeurre@radiantlogic.com>
2025-08-29 18:00:16 +0200

    Building boxplot animation

--- queueless-MC/utils/VisualizationFunctions.py
+++ queueless-MC/utils/VisualizationFunctions.py
@@ -33,9 +33,17 @@ class VisualizationFunctions:
         return fig, axs
 
     def visualize_timeline(self, fig, axs, index, xdata, ydata, ylabel=None, title=None):
-        axs[index].plot(xdata, ydata, 'k-o', markersize=6)
+        axs[index].plot(xdata, ydata, 'k-', linewidth=1.5)
+        axs[index].plot(xdata, ydata, 'o', linewidth=1.5, color='black', markersize=8)
+        axs[index].plot(xdata, ydata, 'o', linewidth=1.5, color='#E24500', markersize=6)
         if ylabel:
-            axs[index].set_ylabel(ylabel, **self.csfont)
+            axs[index].set_ylabel(ylabel, fontsize=10, **self.csfont)
         if title:
             axs[index].set_title(title, loc='left', fontsize=10, **self.csfont)
+        # ydata_unique = np.unique(ydata, return_index=True)
+        # xdata_reduced = list(set([xdata[x] for x in ydata_unique[1]] + xdata[::5]))
+        xdata_reduced = xdata[::3]
+        axs[index].set_xticks(xdata_reduced)
+        axs[index].set_xticklabels(xdata_reduced, rotation=-45, fontsize=8, ha="left", **self.csfont)
+        # axs[index].grid(axis='x', color='#8C97C4', alpha=0.5, linestyle='-', linewidth=1)
         return fig, axs
